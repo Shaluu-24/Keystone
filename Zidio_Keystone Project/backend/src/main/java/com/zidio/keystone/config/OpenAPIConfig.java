fc3b224058a108dc9f0ad0cd3e215a354148b839
Shalini K <shaluhari2410@gmail.com>
2026-08-04 12:26:53 +0530

    Fix Swagger HTTPS server URL

--- Zidio_Keystone Project/backend/src/main/java/com/zidio/keystone/config/OpenAPIConfig.java
+++ Zidio_Keystone Project/backend/src/main/java/com/zidio/keystone/config/OpenAPIConfig.java
@@ -1,5 +1,26 @@
 package com.zidio.keystone.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class OpenAPIConfig {
-    
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        Server server = new Server();
+        server.setUrl("https://keystone-production-3393.up.railway.app");
+        server.setDescription("Railway Production Server");
+
+        return new OpenAPI()
+                .addServersItem(server)
+                .info(new Info()
+                        .title("Keystone API")
+                        .version("v1")
+                        .description("Field Service Management Platform API"));
+    }
 }
